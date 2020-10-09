c9d737e805b8d32fa80b1df8bd1bca665101af60
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-08 21:57:51 -0300

    feat: Add search feature

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,24 +1,49 @@
-const {age, gender, date} = require('../../lib/utils');
+const {
+  age,
+  gender,
+  date
+} = require('../../lib/utils');
 const Instructor = require('../models/Instructor');
 
 module.exports = {
-  index: (req,res) => {
-    Instructor.all((instructors) => {
-      res.render('instructors/index', { instructors });
-    });
-   
+  index: (req, res) => {
+    const {
+      filter
+    } = req.query;
+
+    if (filter) {
+      Instructor.findBy(filter, (instructors) => {
+        return res.render('instructors/index', {
+          instructors,
+          filter
+        });
+      });
+    } else {
+      Instructor.all((instructors) => {
+        res.render('instructors/index', {
+          instructors
+        });
+
+      })
+    }
   },
-  create: (req,res) => {
+  create: (req, res) => {
     return res.render('instructors/create');
   },
-  post: (req,res) => {
+  post: (req, res) => {
     const keys = Object.keys(req.body);
-    for(key of keys){
-      if(req.body[key]==='')
+    for (key of keys) {
+      if (req.body[key] === '')
         return res.send('Preencha todos os campos!');
     };
 
-    let {avatar_url,birth,name,services,gender} = req.body; // destructuring the object into variables 
+    let {
+      avatar_url,
+      birth,
+      name,
+      services,
+      gender
+    } = req.body; // destructuring the object into variables 
 
     const values = [
       avatar_url,
@@ -28,54 +53,64 @@ module.exports = {
       services,
       date(Date.now()).iso
     ];
-    
-    Instructor.create(values,(instructor) => {
+
+    Instructor.create(values, (instructor) => {
       res.redirect(`/instructors/${instructor.id}`);
     });
 
 
   },
-  show: (req,res) => {
-    let { id } = req.params;
+  show: (req, res) => {
+    let {
+      id
+    } = req.params;
     id = Number(id);
-    
-    Instructor.find(id,(instructor) => {
-      if(!instructor) return res.send('Instructor not found!');
+
+    Instructor.find(id, (instructor) => {
+      if (!instructor) return res.send('Instructor not found!');
 
       const showInstructor = {
         ...instructor, // get all the properties of the object and copy to instructor, but i will rewrite some of the values
-        gender:gender(instructor.gender),
-        age:age(instructor.birth),             
-        services:instructor.services.split(','),        // rewrite the value of services
-        created_at: date(instructor.created_at).format      // rewrite the value of created_at
+        gender: gender(instructor.gender),
+        age: age(instructor.birth),
+        services: instructor.services.split(','), // rewrite the value of services
+        created_at: date(instructor.created_at).format // rewrite the value of created_at
       };
 
-      return res.render('instructors/show',{ instructor:showInstructor });
+      return res.render('instructors/show', {
+        instructor: showInstructor
+      });
     });
-    
+
   },
-  edit: (req,res) => {
-    const { id } = req.params;
+  edit: (req, res) => {
+    const {
+      id
+    } = req.params;
 
     Instructor.find(id, (instructor) => {
-      if(!instructor) return res.send('Instructor not found!');
+      if (!instructor) return res.send('Instructor not found!');
 
       const newInstructor = {
         ...instructor,
-        birth:date(instructor.birth).iso
+        birth: date(instructor.birth).iso
       };
 
-      return res.render('instructors/edit', { instructor:newInstructor }); 
+      return res.render('instructors/edit', {
+        instructor: newInstructor
+      });
     });
   },
-  put: (req,res) => {
-    let { id } = req.body;
-  
+  put: (req, res) => {
+    let {
+      id
+    } = req.body;
+
     const keys = Object.keys(req.body);
-    for(key of keys){
-      if(req.body[key] == '') return res.send('Preencha todos os campos');
+    for (key of keys) {
+      if (req.body[key] == '') return res.send('Preencha todos os campos');
     }
-  
+
     const values = [
       req.body.avatar_url,
       req.body.name,
@@ -83,18 +118,20 @@ module.exports = {
       req.body.gender,
       req.body.services,
       req.body.id
-  ];
+    ];
 
-  Instructor.update(values,() => {
-    res.redirect(`/instructors/${id}`)
-  });
-  
+    Instructor.update(values, () => {
+      res.redirect(`/instructors/${id}`)
+    });
+
   },
-  delete: (req,res) => {
-    const { id } = req.body;
-  
-    Instructor.delete(id,() => {
+  delete: (req, res) => {
+    const {
+      id
+    } = req.body;
+
+    Instructor.delete(id, () => {
       res.redirect('/instructors');
     });
-   }
+  }
 };
