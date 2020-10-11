e539a2b244408ab5fbbfea0644b32fc5cd0d64aa
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-10 23:22:12 -0300

    feat: Add Pagination front script

--- public/script.js
+++ public/script.js
@@ -8,8 +8,38 @@ checkbox.addEventListener('click', () => {
 });
 
 // CURRENT PAGE
-for(item of menuItems){
-  if(currentPage.includes(item.getAttribute('href'))){
+for (item of menuItems) {
+  if (currentPage.includes(item.getAttribute('href'))) {
     item.classList.add('active');
   }
 };
+
+
+/* Pagination */
+let totalPages = 20,
+  selectedPage = 4,
+  pages = [],
+  oldPage
+
+for (let actualPage = 1; actualPage <= totalPages; actualPage++) {
+  const firstAndLastPage = actualPage == '1' || actualPage == totalPages;
+  const pagesAfterSelectedPage = actualPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = actualPage >= selectedPage - 2;
+
+  if (firstAndLastPage || pagesAfterSelectedPage && pagesBeforeSelectedPage) {
+
+    if (oldPage && actualPage - oldPage > 2) {
+      pages.push('...');
+    }
+
+    if (oldPage && actualPage - oldPage == 2) {
+      pages.push(oldPage + 1);
+    }
+
+    pages.push(actualPage);
+
+    oldPage = actualPage;
+  }
+}
+
+console.log(pages);
